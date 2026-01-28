2ab55ff92d710ad99e70d10e8d75320aeb02005c
DIMA <shelestdmytro834@gmail.com>
2026-01-28 13:08:24 +0100

    MEOW

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+int Policz(int a, int b) {
+    return a + b;
+}
+
+int Odejmij(int a, int b) {
+    return a - b;
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    std::cout << "W pierwszym branchu; 2 + 3 = " << Policz(2, 3) << std::endl;
+
+    std::cout << "Wynik odejmowania 10 - 4 = " << Odejmij(10, 4) << std::endl;
+
     return 0;
 }

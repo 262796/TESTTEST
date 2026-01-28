d7e84291969e02ddccfca4db50967cd2a68f737c
Aleksandra Bujnowicz <262796@edu.p.lodz.pl>
2026-01-28 13:26:29 +0100

    main.cpp

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ int Odejmij(int a, int b) {
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
-    std::cout << "W pierwszym branchu; 2 + 3 = " << Policz(2, 3) << std::endl;
+    std::cout << "W pierwszym branchu; 2 * 3 = " << Policz(2, 3) << std::endl;
 
-    std::cout << "Wynik odejmowania 10 - 4 = " << Odejmij(10, 4) << std::endl;
+    std::cout << "Wynik odejmowania 10  4 = " << Odejmij(10, 4) << std::endl;
 
     return 0;
 }
